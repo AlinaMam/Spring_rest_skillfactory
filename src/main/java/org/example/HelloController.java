9e865c9080ba7e0a09e3122402c918004fcd04a1
Alina <alinaamamontova@gmail.com>
2024-04-16 14:13:53 +0500

    Добавлен swagger

--- src/main/java/org/example/HelloController.java
+++ src/main/java/org/example/HelloController.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.swagger.annotations.ApiOperation;
 import org.example.dto.StudentData;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,16 +8,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/hello")
 public class HelloController {
     @GetMapping("/student")
+    @ApiOperation(value = "Приветствие студента")
     public String helloStudent() {
         return "Hello, student!!!";
     }
 
     @RequestMapping("/greetings/{name}")
+    @ApiOperation(value = "Приветствие студента по имени")
     public String greetingsMyName(@PathVariable("name") String name) {
         return "Hello, " + name;
     }
 
     @RequestMapping("/submit")
+    @ApiOperation(value = "Обратная связь по оценке студента")
     public String giveMeFeedBackAboutGrade(@RequestBody StudentData studentData) {
         return "Your grade = " + studentData.getGrade();
     }
--- src/main/java/org/example/dto/StudentData.java
+++ src/main/java/org/example/dto/StudentData.java
@@ -1,15 +1,19 @@
 package org.example.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 @Data
 public class StudentData implements Serializable {
     @JsonProperty("firstname")
+    @ApiModelProperty(value = "Имя студента")
     private String firstname;
+    @ApiModelProperty(value = "Фамилия студента")
     @JsonProperty("lastname")
     private String lastname;
+    @ApiModelProperty(value = "Оценка студента")
     @JsonProperty("grade")
     private int grade;
 }
